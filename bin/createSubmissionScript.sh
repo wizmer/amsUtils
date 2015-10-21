1ecaf5497e7ebb0681ebbb2c669e8123661dfff5
wizmer <ben.coste@gmail.com>
2015-10-21 18:37:14 +0200

    createSubmissionScript.sh now check that all required librairies are copied to worker node before launching the jobs

--- bin/createSubmissionScript.sh
+++ bin/createSubmissionScript.sh
@@ -22,36 +22,35 @@ function setupEosRoot {
     /afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select -b fuse mount $eosRoot
 }
 
-function mainFunction {
-    scriptPath=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-    if [[ -f "$HOME/.defaultValueSubmissionScript.sh" ]]; then
-	source $HOME/.defaultValueSubmissionScript.sh
-    fi
-    initial=`echo $HOME | awk -F'/' '{print $5}'`
+function promptTaskName {
     echo Enter the task name:
-    read name
-    absoluteFolderPath=`pwd`/$name
+    read homeDir
+    absoluteFolderPath=`pwd`/$homeDir
     while true; do
-	if [ -d "$name" ]; then
-	    echo The name: $name, is alread used !
+	if [ -d "$homeDir" ]; then
+	    echo The name: $homeDir, is alread used !
 	    echo Please provide an other one:
-	    read name
+	    read homeDir
 	else
 	    break
 	fi
     done
+}
 
-    mkdir $name
+function createHomeDir {
+    mkdir $homeDir
     if [ "$?" == "0" ]; then
-	echo Directory: $name created
+	echo Directory: $homeDir created
     else
-	echo Directory: $name not created !
+	echo Directory: $homeDir not created !
 	echo Exit !
 	return
     fi
     echo ""
+}
 
+
+function promptExecutable {
     echo Enter the executable absolute path to process on workers:
     if [ ! -z "$defaultExecutable" ]; then
 	echo Press Enter for default value: $defaultExecutable
@@ -76,10 +75,49 @@ function mainFunction {
 	    break
 	fi
     done
-    echo Executable path set to: $executable
+    echo "Executable path set to: ${executable}"
     echo ""
-    defaultExecutable=$executable
+    defaultExecutable=${executable}
+}
 
+function promptLibrairies {
+    echo "Enter the list of shared object dependancies"
+    echo "(absolute path or relative from : $(pwd)"
+
+    if [ ! -z "$defaultLibs" ]; then
+	echo "Press Enter for default value: $defaultLibs"
+    fi
+    read libs
+
+    if [[ ! -z "$defaultLibs" && "$libs" == "" ]]; then
+	libs=$defaultLibs
+    fi
+
+    libs=(${libs[@]})
+
+    for i in "${!libs[@]}"; do
+        while true; do
+	    if [[ ! -f "${libs[$i]}" ]]; then
+	        echo "The librairy : ${libs[$i]} does not exist"
+	        echo "Do you want to continue anyway [Y/n] ?"
+	        read confirmation
+	        if [[ "$confirmation" == "y" || "$confirmation" == "Y" || "$confirmation" == "" ]]; then
+		    break;
+	        else
+		    echo Please provide the new path for this librairy:
+		    read libs[$i]
+	        fi
+	    else
+	        break
+	    fi
+        done
+        echo "adding lib : ${libs[$i]}"
+    done
+
+    defaultLibs=${libs[@]}
+}
+
+function promptQueueName {
     echo "Enter the queue name"
     if [ ! -z "$defaultQueue" ]; then
 	echo Press Enter for default queue: $defaultQueue
@@ -91,7 +129,9 @@ function mainFunction {
     echo queue set to: $queue
     echo ""
     defaultQueue=$queue
+}
 
+function promptInputFilesPath {
     echo Enter the directory path where input ROOT files are stored:
     if [ ! -z "$defaultInputFileDir" ]; then
 	echo Press Enter for default path: $defaultInputFileDir
@@ -123,7 +163,9 @@ function mainFunction {
     echo The following directory will be used: $inputFileDir
     echo ""
     defaultInputFileDir=$inputFileDir
+}
 
+function promptChunkSize {
     echo "Enter the chunk size (to process a number of 'chunk' files per run)"
     if [ ! -z "$defaultChunkSize" ]; then
 	echo Press Enter for default chunk size: $defaultChunkSize
@@ -135,7 +177,9 @@ function mainFunction {
     echo chunkSize set to: $chunkSize
     echo ""
     defaultChunkSize=$chunkSize
+}
 
+function promptMaxJobsNumber {
     echo Enter the maximum number of jobs to launch:
     if [ ! -z "$defaultMaxJob" ]; then
 	echo Press Enter for default maximum number of jobs: $defaultMaxJob
@@ -147,7 +191,9 @@ function mainFunction {
     echo maxJob set to: $maxJob
     echo ""
     defaultMaxJob=$maxJob
+}
 
+function promptCreateOutputFolders {
     echo "Create one dedicated folder in your /afs/cern.ch/work/$initial/$USER directory [y/n] ?"
     if [ ! -z "$defaultCreateWorkFolder" ]; then
 	echo Press Enter for default : $defaultCreateWorkFolder
@@ -161,11 +207,11 @@ function mainFunction {
 	echo no folder created
     else
 	createWorkFolder=1
-	mkdir /afs/cern.ch/work/$initial/$USER/$name
+	mkdir /afs/cern.ch/work/$initial/$USER/$homeDir
 	if [ "$?" == "0" ]; then
-	    echo Directory: /afs/cern.ch/work/$initial/$USER/$name created
+	    echo Directory: /afs/cern.ch/work/$initial/$USER/$homeDir created
 	else
-	    echo Directory: /afs/cern.ch/work/$initial/$USER/$name not created !
+	    echo Directory: /afs/cern.ch/work/$initial/$USER/$homeDir not created !
 	    echo Exit !
 	    return
 	fi
@@ -175,7 +221,7 @@ function mainFunction {
 	    echo "Nothing to be copied in work"
 	    toBeCopiedInWorkCommand=""
 	else
-	    toBeCopiedInWorkCommand="cp -R $toBeCopiedInWork /afs/cern.ch/work/$initial/$USER/$name/\${jobName}"
+	    toBeCopiedInWorkCommand="cp -R $toBeCopiedInWork /afs/cern.ch/work/$initial/$USER/$homeDir/\${jobName}"
 	    echo The following command will be executed at the end of each job
 	    echo $toBeCopiedInWorkCommand
 	fi
@@ -199,7 +245,7 @@ function mainFunction {
 	if [ -z "$isEosRootSetUp" ]; then
 	    setupEosRoot
 	fi
-	eosDirName=$eosRoot/ams/user/$initial/$USER/$name
+	eosDirName=$eosRoot/ams/user/$initial/$USER/$homeDir
 	mkdir $eosDirName
 	if [ "$?" == "0" ]; then
 	    echo Directory: $eosDirName created
@@ -212,7 +258,7 @@ function mainFunction {
             echo "Nothing to be copied on EOS"
             toBeCopiedInEosCommand=""
 	else
-            toBeCopiedInEosCommand="cp -R $toBeCopiedInEos \${eosRoot}/ams/user/$initial/$USER/$name/\${jobName}"
+            toBeCopiedInEosCommand="cp -R $toBeCopiedInEos \${eosRoot}/ams/user/$initial/$USER/$homeDir/\${jobName}"
             echo The following command will be executed at the end of each job
             echo $toBeCopiedInEosCommand
 	fi
@@ -231,19 +277,28 @@ function mainFunction {
 	echo The following command will be executed at the end of each job
 	echo $toBeCopiedInMainFolderCommand
     fi
+}
 
-# name=testForGeneral
-# executable=/afs/cern.ch/user/b/bcoste/ecal/equalization/bin/rearleakagel
-# queue=ams1nd
-# inputFileDir=/afs/cern.ch/work/b/bcoste
-# chunkSize=10
-# maxJob=1
-# createWorkFolder=1
-# createEosFolder=1
-# absoluteFolderPath=`pwd`/$name
-# eosRoot=${HOME}/eos
+function mainFunction {
+    scriptPath=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-    Name="$(tr '[:lower:]' '[:upper:]' <<< ${name:0:1})${name:1}"
+    if [[ -f "$HOME/.defaultValueSubmissionScript.sh" ]]; then
+	source $HOME/.defaultValueSubmissionScript.sh
+    fi
+    initial=`echo $HOME | awk -F'/' '{print $5}'`
+
+    promptTaskName
+    createHomeDir
+    cd ${homeDir}
+    promptExecutable
+    promptLibrairies
+    promptQueueName
+    promptInputFilesPath
+    promptChunkSize
+    promptMaxJobsNumber
+    promptCreateOutputFolders
+
+    Name="$(tr '[:lower:]' '[:upper:]' <<< ${homeDir:0:1})${homeDir:1}"
     cat << EOF > ${absoluteFolderPath}/submit${Name}.sh
 #!/bin/bash
 if [ "\$#" -lt 1 ]; then
@@ -261,51 +316,114 @@ export eosRoot=${eosRoot}
 export executable=${executable}
 files=("${inputFileDir}"/*)
 
-if [[ ! -f "\$executable" ]]; then
-    echo Executable: \${executable} not found !
+if [ "\$#" -gt 1 ]; then
+    echo "New file dir provided : \$2" 
+    files=("\$2"/*.root)
+fi
+
+export libs=(${libs[@]})
+
+if [[ ! -f "\${executable}" ]]; then
+    echo "Executable: \${executable} not found !"
     echo Exit !
     exit
 fi
 
 if [[ -d "${absoluteFolderPath}/\$jobName" ]]; then
-    echo folder: ${absoluteFolderPath}/\$jobName exists !
+    echo "folder: ${absoluteFolderPath}/\$jobName exists !"
     echo Please, choose another job name
     echo Exit !
     exit
 fi
+
 mkdir ${absoluteFolderPath}/\$jobName
+cp \${executable} \$jobName/`basename \${executable}`
+executable=`pwd`/\$jobName/`basename \${executable}`
+
+for lib in \${libs[@]}
+do
+    if [[ ! -f "\$lib" ]]; then
+        echo "lib : \${lib} not found !"
+        exit
+    fi
+    cp \${lib} `pwd`/\$jobName/
+done
+
+# Check if some depdencies have not been provided
+(
+    unset LD_LIBRARY_PATH
+    source \${HOME}/.bashrc
+
+    declare -a libsToCopy
+    declare -a missingLibs
+
+    for lib in \${libs[@]}; do
+        libsToCopy=(\${libsToCopy[@]} \$(basename \${lib}))
+    done
+
+    libsToCheck=\$(ldd \${executable} | grep "not found" | awk '{print \$1}')
+
+    for libToCheck in \${libsToCheck[@]}; do
+        test=0
+        for libToCopy in \${libsToCopy[@]}; do
+            if [ "\${libToCopy}" = "\${libToCheck}" ]; then
+                test=1
+                break
+            fi
+        done
+        if [ "\${test}" = "0" ]; then
+            missingLibs=(\${missingLibs[@]} \${libToCheck})
+        fi
+    done
+
+    if (( \${#missingLibs[@]} > 0 )); then
+        echo "Number of missing shared dependancies : \${#missingLibs[@]}"
+        for missing in \${missingLibs[@]}; do
+            echo "missing : \${missing}"
+        done
+        echo "Please add this libraries to the \\\${libs} variable in this script"
+        exit 1
+    fi
+    exit 0
+)
+
+
+if [ "\$?" != "0" ]; then
+    exit
+fi
+
 
 if [[ \${isWorkFolder} == "1" ]]; then
-    if [[ ! -d "/afs/cern.ch/work/$initial/$USER/${name}" ]]; then
-        echo folder: /afs/cern.ch/work/$initial/$USER/${name} does not exist !
+    if [[ ! -d "/afs/cern.ch/work/$initial/$USER/${homeDir}" ]]; then
+        echo folder: /afs/cern.ch/work/$initial/$USER/${homeDir} does not exist !
         echo Exit !
     fi
 
-    if [[ -d "/afs/cern.ch/work/$initial/$USER/${name}/\$jobName" ]]; then
-        echo folder: /afs/cern.ch/work/$initial/$USER/${name}/\$jobName exists !
+    if [[ -d "/afs/cern.ch/work/$initial/$USER/${homeDir}/\$jobName" ]]; then
+        echo folder: /afs/cern.ch/work/$initial/$USER/${homeDir}/\$jobName exists !
         echo Please, choose another job name
         echo Exit !
         exit
     fi
-    mkdir /afs/cern.ch/work/$initial/$USER/${name}/\$jobName
+    mkdir /afs/cern.ch/work/$initial/$USER/${homeDir}/\$jobName
 fi
 
 if [[ \${isEosFolder} == "1" ]]; then
     /afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select -b fuse mount \$eosRoot
 
-    if [[ ! -d "\${eosRoot}/ams/user/$initial/$USER/$name" ]]; then
-        echo folder: \${eosRoot}/ams/user/$initial/$USER/$name does not exist or is not accessible !
+    if [[ ! -d "\${eosRoot}/ams/user/$initial/$USER/$homeDir" ]]; then
+        echo folder: \${eosRoot}/ams/user/$initial/$USER/$homeDir does not exist or is not accessible !
         echo Exit !
         exit
     fi
 
-    if [[ -d "\${eosRoot}/ams/user/$initial/$USER/$name/\$jobName" ]]; then
-        echo folder: \${eosRoot}/ams/user/$initial/$USER/$name/\$jobName exists or is not accessible !
+    if [[ -d "\${eosRoot}/ams/user/$initial/$USER/$homeDir/\$jobName" ]]; then
+        echo folder: \${eosRoot}/ams/user/$initial/$USER/$homeDir/\$jobName exists or is not accessible !
         echo Please, choose another job name
         echo Exit !
         exit
     fi
-    mkdir \${eosRoot}/ams/user/$initial/$USER/$name/\$jobName
+    mkdir \${eosRoot}/ams/user/$initial/$USER/$homeDir/\$jobName
 fi
 
 if [[ ! -f "${absoluteFolderPath}/job${Name}.sh" ]]; then
@@ -319,6 +437,7 @@ export isWorkFolder=\$isWorkFolder
 export isEosFolder=\$isEosFolder
 export eosRoot=\$eosRoot
 export executable=\$executable
+\$(declare -p libs)
 END
 
 for ((i=0; i < \${#files[@]}; i+=chunkSize)); do
@@ -328,7 +447,7 @@ for ((i=0; i < \${#files[@]}; i+=chunkSize)); do
 	break
     fi    
 
-    bsubCommand="bsub -J \${jobName}_\${j} -q \${queue} ${absoluteFolderPath}/job${Name}.sh \${jobName} \${j}"
+    bsubCommand="bsub -J \${jobName}_\${j} -q \${queue} k5reauth -R -- ${absoluteFolderPath}/job${Name}.sh \${jobName} \${j}"
     \$bsubCommand
     echo "\${bsubCommand}, return code: ">>${absoluteFolderPath}/\${jobName}/jobList.log
     echo \${ROOTUPLES}>>${absoluteFolderPath}/\${jobName}/inputFileList.log
@@ -353,14 +472,15 @@ if [ "\$#" -lt 2 ]; then
     exit
 fi
 
-
-k5reauth -R
 klist
 
+homeDir=\$(dirname \${executable})
+
 # Make sure sourcing does not change your path
-curPATH=\`pwd\`
+pushd .
 source \$HOME/.bashrc
-cd \$curPATH
+popd
+source \${homeDir}/env.sh
 
 jobName=\$1
 jobNumber=\$2
@@ -370,6 +490,13 @@ eosRoot=\`pwd\`/eos
 echo making local eos mounting point ...
 /afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select -b fuse mount \${eosRoot}
 cp \${executable} main
+
+for lib in \${libs[@]}
+do
+    cp \${homeDir}/\$(basename \${lib}) .
+done
+
+LD_LIBRARY_PATH=.:${LD_LIBRARY_PATH}
 ls -lrt
 
 for inputFile in \${ROOTUPLES[@]}; do
@@ -384,25 +511,25 @@ for inputFile in \${ROOTUPLES[@]}; do
     else
         ./main \${inputFile}
     fi
+
+    if [[ "\$isWorkFolder" == "1" ]]; then
+        if [[ "$toBeCopiedInWorkCommand" != "" ]]; then
+            $toBeCopiedInWorkCommand
+            code=\${code} || \$?
+        fi
+    fi
+    
+    if [[ "\$isEosFolder" == "1" ]]; then
+        if [[ "$toBeCopiedInEosCommand" != "" ]]; then
+            $toBeCopiedInEosCommand
+            code=\${code} || \$?
+        fi
+        code=\${code} || \$?
+    fi
+
 done
 
-
 code=$?
-
-if [[ "\$isWorkFolder" == "1" ]]; then
-    if [[ "$toBeCopiedInWorkCommand" != "" ]]; then
-        $toBeCopiedInWorkCommand
-        code=\${code} || \$?
-    fi
-fi
-
-if [[ "\$isEosFolder" == "1" ]]; then
-    if [[ "$toBeCopiedInEosCommand" != "" ]]; then
-        $toBeCopiedInEosCommand
-        code=\${code} || \$?
-    fi
-    code=\${code} || \$?
-fi
 
 if [[ "$toBeCopiedInMainFolderCommand" != "" ]]; then
     $toBeCopiedInMainFolderCommand
@@ -425,6 +552,7 @@ function saveDefaultValue {
 
     cat << EOF > $HOME/.defaultValueSubmissionScript.sh
 defaultExecutable=$defaultExecutable        
+defaultLibs="$defaultLibs"
 defaultQueue=$defaultQueue
 defaultInputFileDir=$defaultInputFileDir
 defaultChunkSize=$defaultChunkSize
@@ -432,7 +560,6 @@ defaultMaxJob=$defaultMaxJob
 defaultCreateWorkFolder=$defaultCreateWorkFolder
 defaultCreateEosFolder=$defaultCreateEosFolder
 EOF
-
 }
 
 mainFunction
